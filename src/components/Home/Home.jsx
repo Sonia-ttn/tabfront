405bdcdbfe7c6a49fa6239b4f76141aeeea07b3c
Sonia <sonia.brahman@tothenew.com>
2022-09-23 20:15:51 +0530

    login

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import logo from "./logoo.png";
+import styles from "./Home.module.css";
+
+import {Navigate, useNavigate} from "react-router-dom";
 
 function Home() {
-  return (
-    <div>
-      HOme
+      
+    return (
+       <div className={styles.container}>
+        <div className={styles.top}>
+             <img src={logo} className={styles.img} alt="logo here"/>
+
+            <h1 className={styles.head}>SGM Configurator</h1>
+            <a href="/dash">
+            <button className={styles.green_btn}>LOGIN via TAB
+            </button>
+             </a>
+        </div>
+              
+        
     </div>
+   // <button className={styles.green_btn} onClick={navToDash}>LOGIN via TAB</button>
+          
   )
 }
 
